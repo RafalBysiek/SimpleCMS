36361ba0e09e0f7c3774b5bc88e4f7ac6696d02a
Gabriel Moskal <gab.mos92@gmail.com>
2017-07-10 09:50:59 +0200

    resolved conflicts

--- routes.php
+++ routes.php
@@ -9,9 +9,5 @@ $router->post('users', 'UsersController@store');
 $router->get('login', 'LoginController@showLogin');
 $router->post('login', 'LoginController@processLogin');
 $router->get('add_company', 'PagesController@add_company');
-<<<<<<< HEAD
-$router->post('add_company', 'PagesController@add_company');
-
-=======
-$router->post('logout', 'LoginController@logout');
->>>>>>> 6a2a1e6d6e432b13a15e0e333f98b51b51739a8f
+$router->get('logout', 'LoginController@logout');
+$router->post('add_company', 'Add_companyController@AddingCompany');
